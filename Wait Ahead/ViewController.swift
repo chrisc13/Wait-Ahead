6d6948b9135d6ffdcd45809c02d15767e33bcdc1
Chris Carbajal <43799053+chrisc13@users.noreply.github.com>
2020-06-26 16:23:43 -0700

    Initial Commit

---
+++ Wait Ahead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wait Ahead
+//
+//  Created by Chris Carbajal on 6/26/20.
+//  Copyright © 2020 Chris Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
